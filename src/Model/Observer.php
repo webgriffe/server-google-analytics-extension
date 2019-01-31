ff5cc00a49779cae22f09a076735669f5aa2e6c5
Andrea Zambon <azambon@webgriffe.com>
2019-01-31 09:54:33 +0100

    Avoids useless loggings when saving an order that already has the original client id saved

--- src/Model/Observer.php
+++ src/Model/Observer.php
@@ -42,16 +42,18 @@ class Webgriffe_ServerGoogleAnalytics_Model_Observer
 
         /** @var Mage_Sales_Model_Order $order */
         $order = $observer->getData('order');
+        if ($this->helper->getClientId($order)) {
+            //Nothing to do
+            return;
+        }
+
         $clientId = Mage::app()->getRequest()->getCookie(self::GA_COOKIE_NAME, null);
         if ($clientId) {
             //The first user who saves an order is the customer who submits it by clicking the "place order" button
-            //in the checkout page. So only set this value the first time and never overwrite it if it is already
-            //there.
-            if (!$this->helper->getClientId($order)) {
-                $this->helper->log("Saving client ID '{$clientId}' on order '{$order->getIncrementId()}'");
-                $this->helper->setClientId($order, $clientId);
-                $this->helper->log('Saving done');
-            }
+            //in the checkout page. So only set this value the first time and never overwrite it.
+            $this->helper->log("Saving client ID '{$clientId}' on order '{$order->getIncrementId()}'");
+            $this->helper->setClientId($order, $clientId);
+            $this->helper->log('Saving done');
         } else {
             $this->helper->log("No client ID found for order '{$order->getIncrementId()}'");
         }
